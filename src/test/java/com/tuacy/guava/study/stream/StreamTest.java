00d9246afc5fb18547ce0a1b244c8ea79d3d621a
wuyx <1007178106@qq.com>
2019-07-16 21:26:28 +0800

    stream

---
+++ src/test/java/com/tuacy/guava/study/stream/StreamTest.java
@@ -0,0 +1,126 @@
+package com.tuacy.guava.study.stream;
+
+import com.google.common.collect.Lists;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.function.Consumer;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * @name: StreamTest
+ * @author: tuacy.
+ * @date: 2019/7/16.
+ * @version: 1.0
+ * @Description:
+ */
+public class StreamTest {
+
+    @Test
+    public void collectionStream() {
+
+        List<Integer> list = Lists.newArrayList(1, 2, 3, 4, 5);
+        // 使用List创建一个流对象
+        Stream<Integer> stream = list.stream();
+        // TODO: 对流对象做处理
+    }
+
+    @Test
+    public void collectionParallelStream() {
+
+        List<Integer> list = Lists.newArrayList(1, 2, 3, 4, 5);
+        // 使用List创建一个流对象
+        Stream<Integer> stream = list.parallelStream();
+        // TODO: 对流对象做处理
+    }
+
+    @Test
+    public void arrayStream() {
+
+        int[] intArray = new int[10];
+        for (int index = 0; index < intArray.length; index++) {
+            intArray[index] = index;
+        }
+        // 使用数组创建一个流对象
+        IntStream stream = Arrays.stream(intArray);
+        // TODO: 对流对象做处理
+    }
+
+    @Test
+    public void bufferedReaderStream() {
+
+        File file = new File("/home/tuacy/github/google-guava-study/src/main/resources/application.yml");
+        try {
+            // 把文件里面的内容一行一行的读出来
+            BufferedReader in = new BufferedReader(new FileReader(file));
+            // 生成一个Stream对象
+            Stream<String> stream = in.lines();
+            // TODO: 对流对象做处理
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void fileWalkStream() {
+        Path path = Paths.get("D:\\job\\git\\google-guava-study\\src\\main\\resources");
+        try {
+            // 第二个参数用于指定遍历几层
+            Stream<Path> stream = Files.walk(path, 2);
+            // TODO: 对流对象做处理
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void fileLineStream() {
+        Path path = Paths.get("D:\\job\\git\\google-guava-study\\src\\main\\resources\\application.yml");
+        try {
+            // 生成一个Stream对象
+            Stream<String> stream = Files.lines(path);
+            // TODO: 对流对象做处理
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void fileFindStream() {
+        Path path = Paths.get("D:\\job\\git\\google-guava-study\\src\\main\\resources");
+        try {
+            // 找到指定path下的所有不是目录的文件
+           Stream<Path> stream = Files.find(path, 2, (path1, basicFileAttributes) -> {
+               // 过滤掉目录文件
+               return !basicFileAttributes.isDirectory();
+           });
+            // TODO: 对流对象做处理
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void fileListStream() {
+        Path path = Paths.get("D:\\job\\git\\google-guava-study\\src\\main\\resources");
+        try {
+            // 找到指定path下的所有的文件
+            Stream<Path> stream = Files.list(path);
+            // TODO: 对流对象做处理
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
